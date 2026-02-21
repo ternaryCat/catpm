9d9edec43984a89e0e2b12e877b0d6e1f260f24f
TernaryCat <andrewkarelskiy@gmail.com>
2026-02-22 00:52:45 +0600

    remove heavy counts on ui

--- app/controllers/catpm/system_controller.rb
+++ app/controllers/catpm/system_controller.rb
@@ -7,9 +7,6 @@ module Catpm
       @buffer_size = Catpm.buffer&.size || 0
       @buffer_bytes = Catpm.buffer&.current_bytes || 0
       @config = Catpm.config
-      @bucket_count = Catpm::Bucket.count
-      @sample_count = Catpm::Sample.count
-      @error_count = Catpm::ErrorRecord.count
       @oldest_bucket = Catpm::Bucket.minimum(:bucket_start)
       @active_error_count = Catpm::ErrorRecord.unresolved.count
     end
