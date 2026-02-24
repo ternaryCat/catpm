db2ad25f4cca077382f295d14cc8f1bec62568d1
TernaryCat <andrewkarelskiy@gmail.com>
2026-02-25 01:33:59 +0600

    setup http auth

--- app/controllers/catpm/application_controller.rb
+++ app/controllers/catpm/application_controller.rb
@@ -2,7 +2,22 @@
 
 module Catpm
   class ApplicationController < ActionController::Base
+    before_action :authenticate!
+
     private
+
+    def authenticate!
+      if Catpm.config.access_policy
+        unless Catpm.config.access_policy.call(request)
+          render plain: "Unauthorized", status: :unauthorized
+        end
+      elsif Catpm.config.http_basic_auth_user.present? && Catpm.config.http_basic_auth_password.present?
+        authenticate_or_request_with_http_basic("Catpm") do |username, password|
+          ActiveSupport::SecurityUtils.secure_compare(username, Catpm.config.http_basic_auth_user) &
+            ActiveSupport::SecurityUtils.secure_compare(password, Catpm.config.http_basic_auth_password)
+        end
+      end
+    end
 
     def remembered_range
       if params[:range].present?
